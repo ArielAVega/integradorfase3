79a59b484aec6bb22f09817ab24fdaaf60c65195
ariel <ariel@Vegames>
2024-02-15 11:00:02 -0300

    Creación del proyecto Maven STS, incluye la configuración inicial

---
+++ src/main/java/com/vegasystems/Integradorfase3Application.java
@@ -0,0 +1,13 @@
+package com.vegasystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integradorfase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integradorfase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vegasystems/Integradorfase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegasystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradorfase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
