09e4db4ec68f7a8e01db4ce312d497d4bab3beab
ArielAVega <ariel.vega@educacionit.com>
2024-02-16 07:59:11 -0300

    UPdate entityes [

--- src/main/java/com/vegasystems/entity/Genero.java
+++ src/main/java/com/vegasystems/entity/Genero.java
@@ -1,11 +1,27 @@
 package com.vegasystems.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "generos")
 public class Genero {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "gen_id")
 	private Integer id;
+	@Column(name = "gen_nombre",nullable = false,length = 15)
 	private String nombreGenero;
-
+	@ManyToMany(mappedBy = "generos")
+	private List<Pelicula> peliculas;
 }
 
--- src/main/java/com/vegasystems/entity/ImagenPelicula.java
+++ src/main/java/com/vegasystems/entity/ImagenPelicula.java
@@ -1,12 +1,30 @@
 package com.vegasystems.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "imagenes")
 public class ImagenPelicula {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "img_id")
 	private Integer id;
+	@Column(name = "img_nombre_archivo",nullable = false,length = 100)
 	private String nombreArchivo;
+	@Lob
+	@Column(name = "img_imagen",columnDefinition = "LONGBLOB",nullable = false)
 	private byte[] imagen;
+	@OneToOne(mappedBy = "imagenPelicula")
+	private Pelicula pelicula;
 			
 }
 
